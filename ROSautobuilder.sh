3063393e88206db142af56935d429211feb5631f
slavikyd <slavikyd@gmail.com>
2023-05-01 18:06:46 +0300

    alpha 0.1

--- ROSautobuilder.sh
+++ ROSautobuilder.sh
@@ -13,41 +13,41 @@ then
     if [[ $choice == 'yes' ]] || [[ $choice == 'y' ]] || [[ $choice == 'да' ]] || [[ $choice == 'д' ]]
     then
 
-        # sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+        sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
-        # sudo apt install -y curl
+        sudo apt install -y curl
 
-        # curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+        curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
-        # sudo apt update
+        sudo apt update
 
-        # sudo apt install -y ros-noetic-desktop-full
+        sudo apt install -y ros-noetic-desktop-full
 
-        # apt search ros-noetic
+        apt search ros-noetic
 
-        # source /opt/ros/noetic/setup.bash
+        source /opt/ros/noetic/setup.bash
 
-        # echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+        echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 
-        # source ~/.bashrc
+        source ~/.bashrc
 
-        # sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+        sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
 
-        # sudo apt install -y python3-rosdep
+        sudo apt install -y python3-rosdep
 
-        # sudo rosdep init
+        sudo rosdep init
 
-        # rosdep update
+        rosdep update
 
-        # sudo mkdir catkin_ws
+        sudo mkdir catkin_ws
 
-        # cd catkin_ws/
+        cd catkin_ws/
 
-        # mkdir src
+        mkdir src
 
-        # catkin_make
+        catkin_make
 
-        # echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+        echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
 
         echo "Build for Ubuntu 20.04 finished"
 
@@ -66,41 +66,41 @@ then
     then
         echo "Starting"
 
-        # sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+        sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
-        # sudo apt install -y curl
+        sudo apt install -y curl
 
-        # curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+        curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
-        # sudo apt update
+        sudo apt update
 
-        # sudo apt install -y ros-noetic-desktop-full
+        sudo apt install -y ros-noetic-desktop-full
 
-        # apt search ros-noetic
+        apt search ros-noetic
 
-        # source /opt/ros/noetic/setup.bash
+        source /opt/ros/noetic/setup.bash
 
-        # echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+        echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 
-        # source ~/.bashrc
+        source ~/.bashrc
 
-        # sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+        sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
 
-        # sudo apt install -y python3-rosdep
+        sudo apt install -y python3-rosdep
 
-        # sudo rosdep init
+        sudo rosdep init
 
-        # rosdep update
+        rosdep update
 
-        # sudo mkdir catkin_ws
+        sudo mkdir catkin_ws
 
-        # cd catkin_ws/
+        cd catkin_ws/
 
-        # mkdir src
+        mkdir src
 
-        # catkin_make
+        catkin_make
 
-        # echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+        echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
 
         echo "Build for Ubuntu 18.04 finished"
 
@@ -111,4 +111,25 @@ then
 elif [[ $ver -eq 22 ]]
 then
     echo "Building ROS noetic for 22.04, may be unstable, look after installation logs"
+
+    read -p "Are you ready? (yes\no): " choice
+
+    if [[ $choice == 'yes' ]] || [[ $choice == 'y' ]] || [[ $choice == 'да' ]] || [[ $choice == 'д' ]]
+    then
+        echo "Starting"
+
+        git clone git@github.com:lucasw/ros_from_src.git
+        
+        mkdir build
+        
+        cd build
+        
+        ROSCONSOLE=https://github.com/ros/rosconsole ../ros_from_src/git_clone.sh
+        
+        sudo ../ros_from_src/dependencies.sh
+        
+        ../ros_from_src/build.sh
+    else
+        echo "Build aborted. Something went wrong"
+    fi
 fi
