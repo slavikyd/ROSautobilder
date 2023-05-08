971de91f987ee90d2f7959ea64a16c9c96b1bd7a
slavikyd <slavikyd@gmail.com>
2023-05-09 00:24:30 +0300

    test creation

---
+++ test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+test_file=$1
+
+chmod +x $test_file
+
+echo "Test for args started"
+./$test_file > /dev/null
+testOut=`echo $?`
+if [[ $testOut -eq 1 ]]
+then
+    echo "Test passed"
+else
+    echo "Test failed"
+    exit 1
+fi
+
+echo "Main test"
+./$test_file 18 > /dev/null
+testOut=`echo $?`
+if [[ $testOut -eq 0 ]]
+then
+    echo "Test passed"
+else
+    echo "Test failed"
+    exit 1
+fi
+
